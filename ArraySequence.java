53bb285df27e8dd8e315d0b374696451ccb4c63f
bittergourdking <jkang21@stuy.edu>
2020-12-08 12:00:40 -0500

    added second constructor, all seems to be coolio

--- ArraySequence.java
+++ ArraySequence.java
@@ -3,8 +3,7 @@ public class ArraySequence implements IntegerSequence{
   //private variables
   private int currentIndex;
   private int []data;
-  //constructor
-  /*Construct the sequence by copying values from the other array into the data array*/
+  //constructors
   public ArraySequence(int[] other) {
     data = new int[other.length];
     for (int i = 0; i < other.length; i++) {
@@ -13,6 +12,13 @@ public class ArraySequence implements IntegerSequence{
     currentIndex = 0;
   }
 
+  public ArraySequence(IntegerSequence otherseq) {
+    data = new int[otherseq.length()];
+    for (int i = 0; i < otherseq.length(); i++) {
+      data[i] = otherseq.next();
+    }
+    otherseq.reset();
+  }
   //methods
   public void reset() {
     currentIndex = 0;
--- Tester.java
+++ Tester.java
@@ -12,10 +12,19 @@ public class Tester {
     int[]nums = {17, 13, 3, 1};
     IntegerSequence as = new ArraySequence(nums);
     System.out.println(as.length());
-
+    System.out.println();
     System.out.println("ArraySequence(array):");
     while (as.hasNext()) {
       System.out.print(as.next() + ", ");
     }
+    System.out.println();
+
+    IntegerSequence newr = new Range(10,20);
+    IntegerSequence newas = new ArraySequence(newr);
+    System.out.println("ArraySequence(seq):");
+    while(newas.hasNext()){
+      System.out.print(newas.next()+", ");
+    }
+    System.out.println();
   }
 }
